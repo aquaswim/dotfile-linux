d376045f366d4477459ad692b4f1d31280d91a87
Bayu Setiawan (DevPC) <bayusetiawan37@gmail.com>
2022-03-06 16:01:02 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Linux Dot File
+
+My Linux dot files
+
+## Requirement
+1. manjaro-sway
+1. omzsh
+1. dex
+
+## Nice to have
+1. noto-fonts-emoji
+1. noto-fonts-cjk
+1. flameshot
+
+## Scripts
+1. `refresh.sh`: backup all config
---
+++ refresh.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CP="rsync -vrp"
+
+bkpConfig()
+{
+	BKP_CONFIG_FOLDER=$1
+	$CP ~/.config/$BKP_CONFIG_FOLDER .config
+}
+
+
+# this script is to copy all .dotfile to this repo
+bkpConfig sway
+bkpConfig waybar
+$CP ~/.zshrc .
+$CP ~/.profile .
