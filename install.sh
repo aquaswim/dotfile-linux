9d5f649a3f3a69a76193a5e186e3fb82e4196e4a
Bayu Setiawan (DEVPC) <bayusetiawan37@gmail.com>
2023-07-08 20:11:55 +0700

    rename .profile to .zprofile

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ LN="ln -fsv"
 PWD=$(pwd)
 
 $LN $PWD/.zshrc ~/.zshrc
-$LN $PWD/.profile ~/.profile
+$LN $PWD/.zprofile ~/.zprofile
 $LN $PWD/.ideavimrc ~/.ideavimrc
 $LN $PWD/.config/kitty ~/.config/
 $LN $PWD/.config/ulauncher ~/.config/
