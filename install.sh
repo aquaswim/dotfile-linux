d02106c449c33bf292275080e1c2fa62f414f566
Aquaswim <bayusetiawan37@gmail.com>
2023-12-09 18:32:59 +0700

    rename dotfile to normal file so it's not hidden by default

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 LN="ln -fsv"
 PWD=$(pwd)
 
-$LN $PWD/.ideavimrc ~/.ideavimrc
-$LN $PWD/.gitconfig ~/.gitconfig
+$LN $PWD/ideavimrc ~/.ideavimrc
+$LN $PWD/gitconfig ~/.gitconfig
 $LN $PWD/alias ~/.alias
